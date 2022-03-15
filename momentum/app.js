c3ff79dfee0e004372196a89edab21084d04dbef
JeongJoonMo <85468465+JeongJoonMo@users.noreply.github.com>
2022-03-15 19:05:50 +0900

    login update

--- momentum/app.js
+++ momentum/app.js
@@ -1,11 +1,29 @@
 const loginForm = document.querySelector("#login-form");
-
+const greeting = document.querySelector("#greeting");
 const loginInput = loginForm.querySelector("input");
 const loginButton = loginForm.querySelector("button");
 
-function onLoginSubmit(event){
-  event.preventDefault();
-  console.log(event);
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+function paintGreetings(username){
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+function onLoginSubmit(event){
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
+}
+
+
+if(savedUsername === null){
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+}else{
+  paintGreetings(savedUsername);
+}
