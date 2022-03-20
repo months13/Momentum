e41ab3b2442a3817cae911726f6dfe6c19019793
JeongJoonMo <85468465+JeongJoonMo@users.noreply.github.com>
2022-03-20 18:14:53 +0900

    update todo

--- momentum/js/todo.js
+++ momentum/js/todo.js
@@ -2,11 +2,32 @@ const toDoForm = document.querySelector("#todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.querySelector("#todo-list");
 
+const TODOS_KEY = "todos";
+
+let toDos = [];
+
+function saveToDos(){
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos)); 
+}
+
+
+function deleteToDo(event){
+  const li = event.target.parentElement;
+  li.remove();
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+  saveToDos();
+}
+
 function paintToDo(newTodo){
   const todoList_element = document.createElement("li");
+  todoList_element.id = newTodo.id;
   const todoList_span = document.createElement("span");
+  todoList_span.innerText = newTodo.text;
+  const button = document.createElement("button");
+  button.innerText = "X";
+  button.addEventListener("click", deleteToDo);
   todoList_element.appendChild(todoList_span);  
-  todoList_span.innerText = newTodo;
+  todoList_element.appendChild(button);  
   toDoList.appendChild(todoList_element);
 }
 
@@ -14,7 +35,22 @@ function handleToDoSubmit(event){
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";
-  paintToDo(newTodo);
+  const newTodoObject = {
+    text:newTodo,
+    id: Date.now(),
+  }
+  toDos.push(newTodoObject); 
+  paintToDo(newTodoObject);
+  saveToDos();   
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+ 
+if(savedToDos !== null){
+  const parsedToDos = JSON.parse(savedToDos);
+  toDos = parsedToDos;
+  parsedToDos.forEach(paintToDo);
+}
